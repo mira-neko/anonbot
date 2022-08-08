de5ca9376329e12214603e5d4c1cfce2b87dc22a
Mira Luŝajeva <miralushch@gmail.com>
2022-08-09 00:12:19 +0700

    logging

--- main.py
+++ main.py
@@ -21,21 +21,30 @@ async def start(message: types.Message):
 
 @dp.message_handler()
 async def msg(message: types.Message):
+    logging.info(f"Message: {message}")
     if message.text:
+        logging.info(f"Message: *text*")
         if (CHANNEL[1:] + "/") in message.text.split("\n")[0]:
+            logging.info(f"Message: *reply*")
             text = escape("\n".join(message.text.split("\n")[1:]))
             reply = int(message.text.split("\n")[0].split("/")[-1])
         else:
+            logging.info(f"Message: *not reply*")
             text = escape("\n".join(message.text.split("\n")[1:]))
             reply = None
     else:
+        logging.info(f"Message: *not text*")
         text = None
         reply = None
+    logging.info(f"Message: {reply}: {text}")
     if message.photo:
+        logging.info(f"Message: *sending photo*")
         await bot.send_photo(CHANNEL, message.photo, text, "MarkdownV2", reply_to_message_id=reply)
     elif message.text:
+        logging.info(f"Message: *sending text*")
         await bot.send_message(CHANNEL, text, "MarkdownV2", reply_to_message_id=reply)
     elif message.sticker:
+        logging.info(f"Message: *sending sticker*")
         await bot.send_sticker(CHANNEL, message.sticker)
 
 logging.info(f"Token: {TOKEN}")
